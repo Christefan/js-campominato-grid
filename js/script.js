c390213ed3d41186cdb9b393b2eb6b2519890115
Christefan <chriscapod7@gmail.com>
2022-04-26 01:48:55 +0200

    js completo

---
+++ js/script.js
@@ -0,0 +1,117 @@
+/* 
+Generare una griglia di gioco quadrata in cui ogni cella contiene un numero compreso tra 1 e 100.
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+*/
+
+document.getElementById("play").addEventListener("click", startGame);
+
+
+
+
+function startGame() {
+    // 1. 
+    const gridSize= difficultchoice(document.getElementById('difficulty').value);
+    const dimension = dimensionsHeW(gridSize);
+    const gridArray = generateGridNumbers(gridSize);
+   
+
+    // 2. Per ogni numero creo
+
+    const gridContainer = document.querySelector(".container");
+    gridContainer.innerHTML = "";
+
+    for (let i = 0; i < gridArray.length; i++) {
+        const thisNumber = gridArray[i];
+
+        const domElement = generateBox(thisNumber,dimension)
+
+
+
+        // appendo questo elemnto al contenitore
+        gridContainer.append(domElement);
+    }
+}
+
+// scegliamo il range della difficolta inserita
+function difficultchoice(size){
+    if(size === 'facile'){
+    
+        return 100;
+    }else if(size === 'medio'){
+  
+        return 81;
+    }else{
+      
+        return 49;
+    }
+}
+// definisco la dimensione del box in base al range (selezionato tramite la difficolta del gioco)
+function dimensionsHeW(size){
+    if(size === 100){
+    
+        return 10;
+    }else if(size === 81){
+  
+        return 9;
+    }else{
+      
+        return 7;
+    }
+}
+/**
+ * @param {any} gridNumberQuantity -> la quantità di numeri casuli in ordine da 1 al valore dato
+ * @returns {any} -> array di numeri generati
+ */
+function generateGridNumbers(gridNumberQuantity) {
+    // creare l'array
+    const numberArray = [];
+
+
+    // Creao i numeri random tramite while 
+    while (numberArray.length < gridNumberQuantity) {
+        // 1. genero il numero random 
+        const rndNumber = getRndInteger(1, gridNumberQuantity);
+
+        // 2. se il numero non è all'interno dell'array, allora lo pusho
+        if ( !numberArray.includes(rndNumber) ) {
+            numberArray.push(rndNumber);
+        }
+    }
+    
+    return numberArray;
+}
+
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+
+/**
+ * @param {any} number -> il numero che dobbiamo inserire all'interno dell'elemnto html
+ * @returns {any} -> l'elemento html (il mio div)
+ */
+function generateBox(number,size) {
+    // creo un elemnto html
+    const newElement = document.createElement("div");
+
+    newElement.setAttribute("class", "box");
+    newElement.style.width = `calc(100% / ${size})`;
+    newElement.style.height = `calc(100% / ${size})`;
+
+    // agiungo il span con il numero
+    newElement.innerHTML = `<span>${number}</span>`
+
+    // assegno la classe box
+    newElement.classList.add("box");
+
+    // cambio colore quando clicco la box 
+    newElement.addEventListener("click", function () {
+        newElement.style.backgroundColor = "blue";
+    });
+    
+    // ritorno elemnto
+    return newElement;
+}
+
+
